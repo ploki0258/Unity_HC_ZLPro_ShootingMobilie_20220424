cbeeff473689c0d9faa553d71f5b7167ccb4277b
ploki0258 <57485339+ploki0258@users.noreply.github.com>
2022-05-01 12:03:41 +0800

    連線至大廳且隨機進入大廳

--- Unity_HC_ZLPro_ShootingMobilie_20220424/Assets/Scripts/LobbyManager.cs
+++ Unity_HC_ZLPro_ShootingMobilie_20220424/Assets/Scripts/LobbyManager.cs
@@ -1,28 +1,92 @@
 using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;  //引用Phton Pun  API
+using Photon.Realtime; //引用Phton 即時  API
+
 
 /// <summary>
 /// 大廳管理器
 /// 玩家按下對戰按鈕後開始匹配房間
 /// </summary>
-public class LobbyManager : MonoBehaviour
+/// MonoBehaviourPunCallbacks 連線功能能回乎類別
+/// 例如:登入大廳後回乎你指定的程式
+public class LobbyManager : MonoBehaviourPunCallbacks
 {
-    //GameObject 遊戲物件:存放Unity場景內所有物件
-    //SerializeFied 將私人欄位顯示在屬性面板上
-    //Heder 標題，在屬性面板上顯示粗體字標題
-    [SerializeField,Header("連線中畫面")]
-    private GameObject goConnectView;
+	//GameObject 遊戲物件:存放Unity場景內所有物件
+	//SerializeFied 將私人欄位顯示在屬性面板上
+	//Heder 標題，在屬性面板上顯示粗體字標題
+	[SerializeField, Header("連線中畫面")]
+	private GameObject goConnectView;
+	[SerializeField, Header("對戰按鈕")]
+	private Button btnBattle;
+	[SerializeField, Header("連線人數")]
+	private Text textCountPlayer;
 
-    //註解:說明
-    //讓按鈕跟程式溝通之流程
-    //1.提供公開的方法 public Method
-    //2.按鈕在點擊 On Click 後設定呼叫此方法
+	//喚醒事件:播放遊戲時執行一次。初始化設定
+	private void Awake()
+	{
+		//Photon 連線的連線使用設定
+		PhotonNetwork.ConnectUsingSettings();
+	}
 
-    public void StartConnect()
-    {
-        print("開始連線...");
+	//override 允許複寫繼承的父類類別成員
+	//連線至控制台，在ConnectUsingSettinigs 執行後會自動連線
+	public override void OnConnectedToMaster()
+	{
+		base.OnConnectedToMaster();
+		print("<color=yellow>1. 已經進入控制台</color>");
 
-        //遊戲物件，啟動設定(布林值) - true 顯示，false 隱藏
-        goConnectView.SetActive(true);
-    }
-    
+		//Photon 連線.加入大廳
+		PhotonNetwork.JoinLobby();
+	}
+
+	//連線至大廳成功後會執行此方法
+	public override void OnJoinedLobby()
+	{
+		base.OnJoinedLobby();
+		print("<color=yellow>2. 已經進入大廳</color>");
+
+		//對戰按鈕.互動=啟動
+		btnBattle.interactable = true;
+	}
+
+	//註解:說明
+	//讓按鈕跟程式溝通之流程
+	//1.提供公開的方法 public Method
+	//2.按鈕在點擊 On Click 後設定呼叫此方法
+
+	//開始連線對戰
+	public void StartConnect()
+	{
+		print("開始連線...");
+
+		//遊戲物件，啟動設定(布林值) - true 顯示，false 隱藏
+		goConnectView.SetActive(true);
+
+		//Photon連線的加入隨機房間
+		PhotonNetwork.JoinRandomRoom();
+	}
+	public override void OnJoinRandomFailed(short returnCode, string message)
+	{
+		base.OnJoinRandomFailed(returnCode, message);
+		print("<color=red>4. 加入隨機房間失敗</color>");
+		RoomOptions ro = new RoomOptions();
+		ro.MaxPlayers = 5;
+		PhotonNetwork.CreateRoom("", ro);
+	}
+
+	//加入房間
+	public override void OnJoinedRoom()
+	{
+		base.OnJoinedRoom();
+		print("< color = yellow > 5.開房者進入房間 </ color >");
+		int currentCount = PhotonNetwork.CurrentRoom.PlayerCount;
+		int maxCount = PhotonNetwork.CurrentRoom.MaxPlayers;
+
+		textCountPlayer.text = "連線人數" + currentCount + " / " + maxCount;
+	}
+
+
+
+
 }
