eba7639e5385ee8e3cf953a67810967436139934
ploki0258 <57485339+ploki0258@users.noreply.github.com>
2022-06-05 17:02:03 +0800

    Update 添加更新資料庫功能

--- Unity_HC_ZLPro_ShootingMobilie_20220424/Assets/Scripts/DataManager.cs
+++ Unity_HC_ZLPro_ShootingMobilie_20220424/Assets/Scripts/DataManager.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
 using UnityEngine.Networking;
+using TMPro;
 
 namespace JACK
 {
@@ -11,17 +12,21 @@ namespace JACK
     public class DataManager : MonoBehaviour
     {
         //新增部屬作業內的網址，有變更 GAS 都要新增
-        private string gasLink = "https://script.google.com/macros/s/AKfycbyavYtE0zDeTZ7B0dmgMEIBuS0tApCr2Rdm9KQtPojZJx49FC4wgfFxhHZQFippFPRP/exec";
+        private string gasLink = "https://script.google.com/macros/s/AKfycbzKTNIAaykG5lUvyJph5dTLoTZiEAFJtiI1eDvmC3Tp1rHrhKRiPAxMBVGxwogbVXQCBw/exec";
         private WWWForm form;
 
         private Button btnGetData;
         private Text textPlayerName;
+        private TMP_InputField inputField;
 
         private void Start()
         {
             textPlayerName = GameObject.Find("玩家名稱").GetComponent<Text>();
             btnGetData = GameObject.Find("取得玩家資料按鈕").GetComponent<Button>();
             btnGetData.onClick.AddListener(GetGASData);
+
+            inputField = GameObject.Find("更新玩家名稱").GetComponent<TMP_InputField>();
+            inputField.onEndEdit.AddListener(SetGASData);
         }
 
         /// <summary>
@@ -38,13 +43,33 @@ namespace JACK
         private IEnumerator StarGetGASData()
         {
             //新增網頁連線要求(gasLink，表單資料)
-            using (UnityWebRequest www = UnityWebRequest.Post(gasLink,form))
+            using (UnityWebRequest www = UnityWebRequest.Post(gasLink, form))
             {
                 //等待網頁連線要求
                 yield return www.SendWebRequest();
                 //玩家名稱 = 連線要求下載的文字訊息
                 textPlayerName.text = www.downloadHandler.text;
             }
+        }
+
+        private void SetGASData(string value)
+        {
+            form = new WWWForm();
+            form.AddField("method", "設定");
+            form.AddField("playerName", inputField.text);
+
+            StartCoroutine(StarSetGASData());
+        }
+
+        private IEnumerator StarSetGASData()
+        {
+            using (UnityWebRequest www = UnityWebRequest.Post(gasLink, form))
+            {
+                yield return www.SendWebRequest();
+                textPlayerName.text = inputField.text;
+                print(www.downloadHandler);
+            }
+
         }
     }
 }
